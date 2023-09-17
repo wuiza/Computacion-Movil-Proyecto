2d38ca549bcddf09a0ccc5b0de325f8e5a7522e3
SantiagoCortesUSC <144747168+SantiagoCortesUSC@users.noreply.github.com>
2023-09-16 19:11:47 -0500

    Add files via upload

---
+++ Asstes/Scripts/Enemy.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Enemy : MonoBehaviour
+{
+
+
+
+    public float prueba;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        prueba = 52;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Asstes/Scripts/Player.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class Player : MonoBehaviour
+{
+
+
+    public float vidaJugador;
+    public float vidaActual;
+
+    public float pruebaPlayer;
+    public float prueba;
+
+
+    public Enemy enemigo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+       
+
+        Debug.Log("Esto es en player ");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
